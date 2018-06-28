14fbf82c85653b832bd9ec27ff9db9d827963e5b
paco <skykayla915@gmail.com>
2018-06-28 18:28:41 -0400

    upload

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+// $('#aboutPg').on("click", function (){
+//     $('#bio').show();
+//     $('#portfolio').hide();
+//     $('#contactForm').hide();
+// });//about onclick
+
+
+// $('#portfolioPg').on("click", function (){
+//     $('#portfolio').show();
+//     $('#bio').hide();
+//     $('#contactForm').hide();
+// });//port. onclick
+
+// $('#contactPg').on("click", function (){
+//     $('#contactForm').show();
+//     $('#bio').hide();
+//     $('#portfolio').hide();
+// });//contact onclick
+
+
+})//document.ready
